f038d8b216924fff4507ce84291ef4036e87735e
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2015-09-23 10:18:51 -0400

    Fixing indentation

--- js/app.js
+++ js/app.js
@@ -7,31 +7,32 @@
 
 
 $(document).ready(function() {
-
-	var maximum = parseInt(prompt("Please enter a number from 1 to 50"))
-	
-		if (maximum > 50) {alert("your number is too big");
-	    window.location.reload(true);
-		}
-
-		else if (maximum < 1 ) {alert("Nice try pal!");
-	    window.location.reload(true);
-		}
-		else if (isNaN(maximum)) {alert("Please enter a number between 1 and 50");
+	var maximum = parseInt(prompt("Please enter a number from 1 to 50"));
+	if (maximum > 50) {
+		alert("your number is too big");
 		window.location.reload(true);
-		}	
-
-
-	var fizz = true
+	}
+	else if (maximum < 1 ) {
+		alert("Nice try pal!");
+		window.location.reload(true);
+	}
+	else if (isNaN(maximum)) {
+		alert("Please enter a number between 1 and 50");
+		window.location.reload(true);
+	}	
 
 	for (var i = 1; i <= +maximum; i++) {
-		if ((i%3 === 0) && (i%5===0)) { $('.list').append("<li>fizzbuzz</li>");
+		if ((i%3 === 0) && (i%5===0)) {
+			$('.list').append("<li>fizzbuzz</li>");
 		}
-		else if (i%3 === 0) { $('.list').append("<li>fizz</li>");
+		else if (i%3 === 0) {
+			$('.list').append("<li>fizz</li>");
 		}
-		else if (i%5 === 0) { $('.list').append("<li>buzz</li>");
+		else if (i%5 === 0) {
+			$('.list').append("<li>buzz</li>");
 		}
-		else { $('.list').append("<li>" + i + "</li>");
+		else {
+			$('.list').append("<li>" + i + "</li>");
 		}
 	}
 });
